35a6af47de632f94afec3337d3c15e69b292f81b
ObsoleteTheBest <41532789+ObsoleteTheBest@users.noreply.github.com>
2018-07-22 11:59:54 +0530

    Obsolete Bot
    The Best Discord Bot

---
+++ commands/warn.js
@@ -0,0 +1,59 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+module.exports.run = async (bot, message, args) => {
+
+   if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("No can do pal!");
+   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+   if(!wUser) return message.reply("Couldn't find them you");
+   if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
+   let reason = args.slice(1).join(" ") || "None"
+
+   if(!warns[wUser.id]) warns[wUser.id] = {
+     warns: 0
+   };
+
+   warns[wUser.id].warns++;
+
+   fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+     if (err) console.log(err)
+   });
+
+   let warnEmbed = new Discord.RichEmbed()
+   .setDescription("Warns")
+   .setAuthor(message.author.username)
+   .setColor("#00f5be")
+   .addField("Warned User", wUser.tag)
+   .addField("Warned In", message.channel)
+   .addField("Number of Warnings", warns[wUser.id].warns)
+   .addField("Reason", reason);
+
+  let warnchannel= message.guild.channels.find(`name`, "incidents");
+  if(!warnchannel) return message.reply("Couldn't find incidents channel.");
+
+  warnchannel.send(warnEmbed);
+
+  if(warns[wUser.id].warns == 2){
+    let muterole = message.guild.roles.find(`name`, "muted");
+    if(!muterole) return message.reply("You should create that role dude");
+
+    let mutetime = "30m";
+    await(wUser.addRole(muterole.id));
+    message.channel.send(`${wUser.tag} has been temporarily muted.`);
+
+    setTimeout(function(){
+      wUser.removeRole(muterole.id)
+      message.channel.reply(`They have been unmuted.`)
+    })
+  }
+  if(warns[wUser.id].warns == 3){
+    message.guild.member(wUser).ban(reason);
+    message.channel.send(`${wUser.tag} has been banned.`)
+  }
+
+}
+module.exports.help = {
+  name: "warn"
+}
---
+++ commands/warnlevel.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+
+module.exports.run = async (bot, message, args) => {
+
+
+}
+
+module.exports.help = {
+  name: "warnlevel"
+}
